570b4643db65ffd4e3467441239484c8b28468b9
silasmas <silasjmas@gmail.com>
2022-06-29 03:14:13 +0200

    llfd

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -23,7 +23,7 @@
     <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/js/sweetalert/sweetalert.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/summernote/summernote.css') }}" rel="stylesheet">
 
